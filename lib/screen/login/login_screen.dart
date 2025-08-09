911718619796a1563c16b04c0023ec462c8703a1
gunawanbudi930 <gunawanbudi930@gmail.com>
2025-08-09 10:53:12 +0000

    setup firebase

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -13,32 +13,30 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ClipPath(
-            clipper: WaveClipper(),
-            child: SizedBox(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height * 0.5,
-              child: Image.asset(
-                'assets/images/hero.png',
-                fit: BoxFit.cover,
-                opacity: const AlwaysStoppedAnimation(0.85),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipPath(
+              clipper: WaveClipper(),
+              child: SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Image.asset(
+                  'assets/images/hero.png',
+                  fit: BoxFit.cover,
+                  opacity: const AlwaysStoppedAnimation(0.85),
+                ),
               ),
             ),
-          ),
-          Flexible(
-            child: Image.asset(
+            Image.asset(
               "assets/images/logo_header_green.png",
               width: 250,
               height: 140,
             ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
               child: Text(
                 "Create a sticker from the available template here and custom the text",
                 overflow: TextOverflow.ellipsis,
@@ -49,42 +47,43 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                minimumSize: Size(double.infinity, 40),
-                padding: const EdgeInsets.only(
-                  left: 20,
-                  right: 20,
-                  top: 10,
-                  bottom: 10,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  minimumSize: Size(double.infinity, 40),
+                  padding: const EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: 10,
+                    bottom: 10,
+                  ),
+                  backgroundColor:
+                      Theme.of(context).colorScheme.surfaceContainer,
+                  shape: const StadiumBorder(),
                 ),
-                backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
-                shape: const StadiumBorder(),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/images/google_icon.png",
-                    width: 40,
-                    height: 40,
-                  ),
-                  const SizedBox.square(dimension: 10),
-                  Flexible(
-                    child: Text(
-                      'Sign In with Google',
-                      style: Theme.of(context).textTheme.titleMedium,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/google_icon.png",
+                      width: 40,
+                      height: 40,
                     ),
-                  ),
-                ],
+                    const SizedBox.square(dimension: 10),
+                    Flexible(
+                      child: Text(
+                        'Sign In with Google',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
